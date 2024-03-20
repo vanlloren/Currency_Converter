1696a4a5a76d1cb889baf13d4dc28fec78ab61cf
vanlloren <lorenzo.vaninetti@mail.polimi.it>
2024-03-20 23:31:47 +0100

    Scrollbar added to the main menu frame

--- src/main/java/GarbageAndTrials/TrialApp.java
+++ src/main/java/GarbageAndTrials/TrialApp.java
@@ -70,7 +70,17 @@ public class TrialApp {
         JFrame mainMenuFrame = new JFrame("Main Menu");
         mainMenuFrame.setVisible(true);
         mainMenuFrame.setLayout(new BorderLayout());
-        mainMenuFrame.setSize(300, 300);
+        mainMenuFrame.setSize(800, 650);
+
+        JScrollBar scrollBar = new JScrollBar();
+        scrollBar.setVisible(true);
+        mainMenuFrame.add(scrollBar, BorderLayout.EAST);
+        mainMenuFrame.addMouseWheelListener(e -> {
+            int notches = e.getWheelRotation();
+            //muove la scrollbar in base al movimento della rotellina del mouse
+            scrollBar.setValue(scrollBar.getValue() + scrollBar.getBlockIncrement() * notches);
+        });
+
     }
 
     public static void main(String[] args) {
