3ce7e5d178fecaf33ab2efead6f94ad5529541ce
vanlloren <lorenzo.vaninetti@mail.polimi.it>
2024-03-20 23:39:28 +0100

    Created the GUI launcher

---
+++ src/main/java/GUI/GraphicApp.java
@@ -0,0 +1,73 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import static java.awt.BorderLayout.CENTER;
+
+public class GraphicApp {
+    private static final GUIHandler guiHandler = new GUIHandler();
+
+    static void lookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static void GUIInitializer(){
+        lookAndFeel();
+        JDialog startingDialog = new JDialog();
+        startingDialog.setTitle("Welcome");
+        // TODO startingDialog.setIconImage(new ImageIcon("src/main/resources/img.png").getImage());
+
+        startingDialog.setVisible(true);
+        startingDialog.setLayout(new BorderLayout());
+        startingDialog.setSize(500, 150);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+
+        JLabel startingLabel = new JLabel("Welcome to vanlloren's conversion software!");
+        startingLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        startingLabel.setFont(new Font("Arial", Font.BOLD, 12));
+        startingLabel.setVisible(true);
+        panel.add(startingLabel, BorderLayout.NORTH);
+
+        JLabel startingLabel2 = new JLabel("Please click OK to continue.");
+        startingLabel2.setHorizontalAlignment(SwingConstants.CENTER);
+        startingLabel2.setFont(new Font("Arial", Font.BOLD, 12));
+        startingLabel2.setVisible(true);
+        panel.add(startingLabel2, CENTER);
+
+        JButton startButton = new JButton("OK");
+        startButton.setHorizontalAlignment(SwingConstants.CENTER);
+        startButton.setVerticalAlignment(SwingConstants.CENTER);
+        startButton.setVisible(true);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setVisible(true);
+        buttonPanel.setLayout(new FlowLayout());
+
+
+        buttonPanel.add(new JLabel(" "));
+        buttonPanel.add(startButton);
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+        startingDialog.add(panel, CENTER);
+        startButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                startingDialog.dispose();
+                guiHandler.mainMenu();
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+        GUIInitializer();
+    }
+}
