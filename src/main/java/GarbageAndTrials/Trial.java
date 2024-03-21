39eb0a784c3fb14407e76f3783f7cfd5e7af085a
vanlloren <lorenzo.vaninetti@mail.polimi.it>
2024-03-21 14:38:13 +0100

    removed some code snippet and added others

--- src/main/java/GarbageAndTrials/Trial.java
+++ src/main/java/GarbageAndTrials/Trial.java
@@ -1,6 +1,21 @@
 package GarbageAndTrials;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Trial {
 
+    /*codice per creare scrollbar e gestire il movimento della rotellina del mouse
+    JScrollBar scrollBar = new JScrollBar();
+        scrollBar.setVisible(true);
+        mainMenuFrame.add(scrollBar, BorderLayout.EAST);
+        mainMenuFrame.addMouseWheelListener(e -> {
+        int notches = e.getWheelRotation();
+        //muove la scrollbar in base al movimento della rotellina del mouse
+        scrollBar.setValue(scrollBar.getValue() + scrollBar.getBlockIncrement() * notches);
+    });
 
+    */
+
+    //800*650 buona dimensione per la finestra
 }
